1eae971ebd0076afa461ccf2a24fa2024d693871
stevnrd <steven_diep@hotmail.co.uk>
2023-10-28 14:27:08 -0400

    fixed bug with budget

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,8 +58,8 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
-                    <input
+                    <div>£
+                        <input
                         required='required'
                         type='number'
                         id='cost'
@@ -67,6 +67,8 @@ const AllocationForm = (props) => {
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
+                    </div>
+                    
 
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,27 +2,31 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { expenses, budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
     const handleBudgetBlur = () => {
         // hard coded this not sure how to fix it
         // just want to get val from expenseTotal
-        if(newBudget < 500) { 
+        if(newBudget < totalExpenses) { 
             alert("You cannot reduce the budget value lower than the spending");
         }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{newBudget}</span>
+<span>Budget: £{newBudget} </span>
 <input 
     type="number" 
     step="10" 
     value={newBudget} 
     onChange={handleBudgetChange}
     onBlur={handleBudgetBlur}></input>
+    
 </div>
     );
 };
